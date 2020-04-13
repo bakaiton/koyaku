96064b81e3de1ae696d5a00ea36d983756aa2332
kaito <16t2803a@shinshu-u.ac.jp>
2020-04-13 22:26:18 +0900

    ゲーム数の追加

--- app/src/main/java/com/example/koyaku/MainActivity.kt
+++ app/src/main/java/com/example/koyaku/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.koyaku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Button
 
@@ -10,26 +12,50 @@ class MainActivity : AppCompatActivity() {
     var wmNum = 0
     var cherryNum = 0
     var chanceNum = 0
+    var gameNum = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //ゲーム数のボタンを押したときの動作
+        gameCnt.setOnClickListener {
+            if(editText.text.toString().isNotEmpty()) {
+                gameNum = Integer.parseInt(editText.text.toString())
+                if (bellNum != 0) {
+                    bellValue.setText(Integer.toString(gameNum / bellNum))
+                }
+                if (wmNum != 0) {
+                    wmValue.setText(Integer.toString(gameNum / wmNum))
+                }
+                if (chanceNum != 0) {
+                    cherryValue.setText(Integer.toString(gameNum / cherryNum))
+                }
+                if (chanceNum != 0) {
+                    chanceValue.setText(Integer.toString(gameNum / chanceNum))
+                }
+            }
+        }
 
+        //カウンターの動作
         bellButton.setOnClickListener {
             bellNum += 1
             bellCnt.setText(Integer.toString(bellNum))
+            bellValue.setText(Integer.toString(gameNum/bellNum))
         }
         wmButton.setOnClickListener {
             wmNum += 1
             wmCnt.setText(Integer.toString(wmNum))
+            wmValue.setText(Integer.toString(gameNum/wmNum))
         }
         cherryButton.setOnClickListener {
             cherryNum += 1
             cherryCnt.setText(Integer.toString(cherryNum))
+            cherryValue.setText(Integer.toString(gameNum/cherryNum))
         }
         chanceButton.setOnClickListener {
             chanceNum += 1
             chanceCnt.setText(Integer.toString(chanceNum))
+            chanceValue.setText(Integer.toString(gameNum/chanceNum))
         }
     }
 }
